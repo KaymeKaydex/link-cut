957f67b2306d9b6981391fa6269032713ad1c5a7
KaymeKaydex <djassange@yandex.ru>
2021-06-20 21:53:16 +0300

    [OZON-T-0] comments for migration script

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -9,13 +9,19 @@ import (
 	"gorm.io/gorm"
 )
 
+// Создает миграции в базу данных
+
 func main() {
 
+	// Устанавливаем уровень логирования
 	jww.SetLogThreshold(jww.LevelTrace)
 	jww.SetStdoutThreshold(jww.LevelInfo)
 	jww.INFO.Println("Migration started")
+
+	// Запуск фонового контекста
 	ctx := context.Background()
 
+	// Создаем DSN
 	dsn := fmt.Sprintf("host='%s' port='%s' user='%s' password='%s' dbname='%s' sslmode='disable'",
 		"0.0.0.0",
 		"5432",
@@ -23,15 +29,19 @@ func main() {
 		"pass",
 		"link-cut")
 
+	// Создание подключения с базой
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		jww.INFO.Println("Cant open postgers connection", err)
 		return
 	}
+
+	// Создание миграции
 	err = db.WithContext(ctx).AutoMigrate(&model.URLContainer{})
 	if err != nil {
 		jww.INFO.Println("Migration error :", err)
 		return
 	}
+
 	jww.INFO.Println("Successfully migrate")
 }
